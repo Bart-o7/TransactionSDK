adf7e9db1f45cf6ac1ca26c09c11a1e9cffebeab
Bartosz Banachowski <bartoszbanachowski@bnch-software.com>
2024-10-14 10:18:39 +0200

    Initial Commit

---
+++ PayairSDK/PayairSDK.h
@@ -0,0 +1,18 @@
+//
+//  PayairSDK.h
+//  PayairSDK
+//
+//  Created by BNCH SOFTWARE on 14/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayairSDK.
+FOUNDATION_EXPORT double PayairSDKVersionNumber;
+
+//! Project version string for PayairSDK.
+FOUNDATION_EXPORT const unsigned char PayairSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayairSDK/PublicHeader.h>
+
+
